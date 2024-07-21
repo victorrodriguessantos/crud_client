5b151c261969de528ee1524e64b42b6f9f9b2163
Victor <victorrodrigues288@gmail.com>
2024-07-21 02:14:57 -0300

    nova rota adicionada

--- routes.js
+++ routes.js
@@ -17,4 +17,25 @@ router.post('/criarProduto', (req, res) => {
     });
 });
 
+// Rota para atualizar o produto
+router.put('/editarProduto/:id', (req, res) => {
+    const id_produto = req.params.id;
+    const { name_produto, descricao } = req.body;
+
+    if (!id_produto || !name_produto || !descricao) {
+        return res.status(400).json({ error: 'Todos os campos são obrigatórios: id_produto, name_produto, descricao' });
+    }
+
+    db.query('UPDATE Produtos SET name_produto = ?, descricao = ? WHERE id_produto = ?', [name_produto, descricao, id_produto], (err, result) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+        }
+        res.status(200).json({ message: 'Produto atualizado com sucesso!', id_produto, name_produto, descricao });
+    });
+});
+
+
+
+
 export default router;
